c2b5eb6c1261850e3ad9f92f75efde5136861ca7
glossada <gabriellossada@gmail.com>
2025-10-26 14:51:03 -0300

    experience in progress..

--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,11 +1,33 @@
 <template>
     <section class="about">
-    <h1>Experience</h1>
-    <p>Acá va tu descripción, skills, links, etc.</p>
+        <h1>Working expirience</h1>
+        <div class="enterpriseTitle">
+            <img src="/public/images/42i.png" alt="42i Logo" />
+            <h2><a href="https://42i.co/"> - Digital Product Development Company</a></h2>
+        </div>
+        <div>
+            <p>Development and implementation <strong><a href="#back">Bakc-end</a>/<a
+                        href="#front">Front-end</a></strong> of high-traffic websites: Hot Sale, Hot
+                Fashion, Hot Travel, Cyber Days, and Glam Days, in collaboration with
+                chambers of commerce across different countries.</p>
+        </div>
+        <div>
+            <h3 id="back">Back-end</h3>
+            <ul>
+                <li>Creation and maintenance of APIs using Kotling and Spring in main server and Node.js and Express in
+                    web environment.</li>
+                <li>Database management with MySQL.</li>
+                <li>data structure design, endpoints, controllers, API integrations,
+                    and security with JWT.</li>
+                <li>Redis cache creation for consuming in the web side.</li>
+                <li>Implementation of authentication and authorization mechanisms and web tokens.</li>
+            </ul>
+        </div>
+
     </section>
-    </template>
-    
-    
-    <script>
-    export default { name: 'AboutView' };
-    </script>
+</template>
+
+
+<script>
+export default { name: 'AboutView' };
+</script>
